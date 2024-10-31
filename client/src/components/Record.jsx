884993a0d71e2dd80ce5b42ce6fba694719a54b3
Shabbeer <shabbeerpetersen10@gmail.com>
2024-11-01 00:08:50 +0200

    added http requests

--- client/src/components/Record.jsx
+++ client/src/components/Record.jsx
@@ -11,5 +11,136 @@ export default function Record(){
     const params = useParams();
     const navigate = useNavigate();
 
-    
+    useEffect(()=> {
+        async function fetchData() {
+            const id = params.id ?.toString() || undefined;
+            if(!id) return;
+            setIsNew(false);
+            const response = await fetch(
+                `http://localhost:3000/record/${params.id.toString()}`
+            );
+
+            if (!response.ok) {
+                const message = `An error has occurred: ${response.statusText}`;
+                console.error(message);
+                return;
+            }
+
+            const record = await response.json();
+            if (!record) {
+                console.warn(`Record with id ${id} not found`);
+                navigate("/");
+                return;
+            }
+            setForm(Record);
+        }
+        fetchData();
+        return;
+    }, [params.id, navigate]);
+
+    // method to update state
+    function updateForm(value) {
+        return setForm((prev) => {
+            return { ...prev, ...value };
+        });
+    }
+
+    //function to handle submit.
+    async function onsubmit(e) {
+        e.preventDefault();
+        const person = {...form};
+        try {
+            let response;
+            if (isNew) {
+                response = await fetch("http://localhost:3000/record", {
+                    method: "POST",
+                    headers: {'Content-type': 'application/json'},
+                    body: JSON.stringify(person),
+                });
+            } else {
+                response = await fetch(`http://localhost:3000/record/${params.id}`,{
+                    method: "PATCH",
+                    headers: {"Content-Type":"application/json"},
+                    body: JSON.stringify(person),
+                });
+            }
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status code is: ${response.status}`)
+            }
+        } catch (error) {
+            console.error("A problem occurred with your fetch operation: ", error);
+        } finally {
+            setForm({name: "", position: "", level: ""});
+            navigate("/");
+        }
+    }
+
+    return (
+        <>
+            <h3>Create or Update Employee Record</h3>
+            <form
+            onSubmit={onsubmit}>
+                <div>
+                    <h2>Employee Info</h2>
+                    <p>
+                        This info will be displayed publicly.
+                    </p>
+                </div>
+
+                <div>
+                    <div>
+                        <label htmlFor="name">Name</label>
+                        <div>
+                            <div>
+                                <input
+                                type="text"
+                                name= "name"
+                                id = "name"
+                                placeholder="First Last"
+                                value={form.name}
+                                onChange={(e)=> updateForm({name: e.target.value})}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <label htmlFor="position">Position</label>
+                        <div>
+                            <div>
+                                <input
+                                type="text"
+                                name= "position"
+                                id = "position"
+                                placeholder="Developer Advocate"
+                                value={form.position}
+                                onChange={(e)=> updateForm({name: e.target.value})}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <fieldset>
+                            <legend>Position Options</legend>
+                            <div>
+                                <div>
+                                <input
+                                type="radio"
+                                name= "positionOptions"
+                                id = "position"
+                                value= "Intern"
+                                checked= {form.level == "Intern"}
+                                onChange={(e)=> updateForm({name: e.target.value})}
+                                />
+                                <label htmlFor="positionIntern">
+                                    Intern
+                                </label>
+                                </div>
+                            </div>
+                        </fieldset>
+                    </div>
+                </div>
+            </form>
+        </>
+    )
 }
